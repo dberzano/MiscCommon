da32d064f3cd64b12ccef1ad749617bc8d635af6
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-02-28 09:57:19 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@473 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ IXMLPersist.h
@@ -0,0 +1,31 @@
+/************************************************************************/
+/**
+ * @file IXMLPersist.h
+ * @brief IXMLPersist is a persistence interface
+ * @author Anar Manafov A.Manafov@gsi.de
+ */ /*
+
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           2006-07-15
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2006 GSI GridTeam. All rights reserved.
+************************************************************************/
+#ifndef IXMLPERSIST_H
+#define IXMLPERSIST_H
+
+// Xerces-C++ headers
+#include <xercesc/dom/DOMNode.hpp>
+// GAW
+#include "ErrorCode.h"
+
+/** @brief an XML persistence interface*/
+class IXMLPersist
+{
+    public:
+        virtual ERRORCODE Read( xercesc::DOMNode* _element ) = 0;
+        virtual ERRORCODE Write( xercesc::DOMNode* _element ) = 0;
+};
+
+#endif
