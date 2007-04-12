c2ddbd79e8bce6ca5f00cabe36aac50a2a055a68
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-04-12 16:51:05 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@624 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ Process.h
@@ -0,0 +1,23 @@
+/************************************************************************/
+/**
+ * @file Process.h
+ * @brief $$File comment$$
+ * @author Anar Manafov A.Manafov@gsi.de
+ *//*
+
+        version number:     $LastChangedRevision$
+        created by:            Anar Manafov
+                                    2007-04-12
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+
+        Copyright (c) 2007 GSI GridTeam. All rights reserved.
+*************************************************************************/
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+namespace MiscCommon
+{
+    
+};
+
+#endif /*PROCESS_H_*/
