cd20cf9973e8b5a1329cf3f166dc55497b9d9525
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-05-03 17:06:21 +0000

    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@704 47ffe4a8-6206-0410-815e-8f101de37557

--- SysHelper.h
+++ SysHelper.h
@@ -3,20 +3,51 @@
 
 // API
 #include <unistd.h>
+#include <pwd.h>
 
 // OUR
 #include "def.h"
-
-inline void get_cur_user( std::string *_RetVal )
+namespace MiscCommon
 {
-    if ( !_RetVal )
-        return ;
 
-    CHARVector_t Buf(L_cuserid);
+    inline void get_cuser_name( std::string *_RetVal )
+    {
+        if ( !_RetVal )
+            return ;
 
-    cuserid( &Buf[0] );
+        CHARVector_t Buf(L_cuserid);
 
-    *_RetVal = std::string( &Buf[0] );
-}
+        cuserid( &Buf[0] );
 
+        *_RetVal = std::string( &Buf[0] );
+    }
+
+    inline void get_homedir( uid_t _uid, std::string *_RetVal )
+    {
+        if ( !_RetVal )
+            return ;
+
+        passwd *pwd = getpwuid( _uid );
+        *_RetVal = pwd ? std::string( pwd->pw_dir) : "";
+    }
+
+    inline void get_cuser_homedir( std::string *_RetVal )
+    {
+        get_homedir( getuid(), _RetVal );
+    }
+
+    inline void smart_homedir_append( std::string *_Path )
+    {
+        if ( !_Path )
+            return ;
+
+        std::string sHome;
+        get_cuser_homedir( &sHome );
+        smart_append( &sHome, '/');
+
+        replace<std::string>( _Path, "~/", sHome );
+        replace<std::string>( _Path, "$HOME/", sHome );
+    }
+
+};
 #endif /*SYSHELPER_H_*/
