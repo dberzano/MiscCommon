f386c4d59c7dea275a5daf5982324ce728873814
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-06-08 13:01:27 +0000

    added: CHexViewer - helps to represent data as in a HEX editor
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@865 47ffe4a8-6206-0410-815e-8f101de37557

---
+++ HexView.h
@@ -0,0 +1,110 @@
+/************************************************************************/
+/**
+ * @file HexView.h
+ * @brief Helps to represent data as in a HEX viewer
+ * @author Anar Manafov A.Manafov@gsi.de
+ */ /*
+ 
+        version number:    $LastChangedRevision$
+        created by:        Anar Manafov
+                           $$date$$
+        last changed by:   $LastChangedBy$ $LastChangedDate$
+ 
+        Copyright (c) 2007 GSI GridTeam. All rights reserved.
+*************************************************************************/
+#ifndef HEXVIEW_H_
+#define HEXVIEW_H_
+
+// OUR
+#include "def.h"
+
+namespace MiscCommon
+{
+	
+	/**
+	 * @brief This class helps to represent a given contaner's data as in a HEX viewer
+	 * 
+	 * @note Example:
+	 * @code
+	 * 
+	 * int main()
+		{
+    		BYTEVector_t vec;
+    		vec.push_back(125);
+    		vec.push_back(525);
+    		vec.push_back(325);
+    		vec.push_back(123);
+    		vec.push_back(154);
+    		vec.push_back(125);
+    		vec.push_back(125);
+    		..
+    		..
+    		vec.push_back(167);
+    
+    		cout << BYTEVectorHexView_t( vec ) << endl;
+		}
+		
+		Output:
+		
+		0x00000000 | 7D 0D 45 7B 9A 7D 7D 7D 7D 7D 7D 7D 7D 7D A7 7D  | }.E{.}}}}}}}}}.}
+		0x10000000 | 7D 7D 0D 45 7B 9A 7D 7D 7D 7D 7D 7D 7D 7D 7D A7  | }}.E{.}}}}}}}}}.
+		0x20000000 | 7D 7D 0D 45 7B 9A 7D 7D 7D 7D 7D 7D 7D 7D 7D A7  | }}.E{.}}}}}}}}}.
+		0x23000000 | 7D 7D A7
+	 * @endcode
+	 * 
+	 * 
+	 **/
+    template < class _T >
+    class CHexView
+    {
+        public:
+            CHexView( const _T &_Val, size_t _nElementsInRaw = 16 ):
+                    m_nElementsInRaw( _nElementsInRaw ),
+                    m_Container(_Val)
+            {}
+
+            friend std::ostream& operator << ( std::ostream &_ostream, const CHexView<_T> &_this )
+            {
+                std::stringstream ssHex;
+                ssHex << std::hex << std::uppercase;
+                std::stringstream ssTxt;
+                size_t nCount = 0;
+                BYTEVector_t::const_iterator iter = _this.m_Container.begin();
+                BYTEVector_t::const_iterator iter_end = _this.m_Container.end();
+                for (; iter != iter_end; ++iter )
+                {
+                    ssHex << std::setw(2) << std::setfill('0') << ( static_cast<unsigned int>(*iter) ) << ' ';
+                    ssTxt << (isprint(*iter) ? static_cast<char>(*iter) : '.' );
+                    ++nCount;
+                    if ( 0 == (nCount % _this.m_nElementsInRaw) )
+                    {
+                        _this.Print( _ostream, ssHex, ssTxt, nCount );
+                        ssHex.str("");
+                        ssTxt.str("");
+                    }
+                }
+                if ( !ssHex.str().empty() )
+                    _this.Print( _ostream, ssHex, ssTxt, nCount );
+
+                return _ostream;
+            }
+
+        private:
+            void Print( std::ostream &_ostream, const std::stringstream &_ssHex, const std::stringstream &_ssTxt, size_t _nCount ) const
+            {
+                _ostream
+                << "0x" << std::setw(8) << std::setfill('0') << std::hex << std::uppercase << _nCount - m_nElementsInRaw
+                << " | " << std::left << std::setw( m_nElementsInRaw * 3 ) << std::setfill(' ') << _ssHex.str()
+                << " | " << std::left << std::setw( m_nElementsInRaw ) << _ssTxt.str()
+                << '\n';
+            }
+
+        private:
+            const size_t m_nElementsInRaw;
+            const _T &m_Container;
+    };
+
+    typedef CHexView<BYTEVector_t> BYTEVectorHexView_t;
+};
+
+#endif /*HEXVIEW_H_*/
