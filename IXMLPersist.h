77ff259953bd71d2fad32743ed07e7d1a47a1a1b
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-08-08 16:49:02 +0000

    GAW_modified: using new IXMLPersistImpl schema and BEGIN/END_READ_XML_CFG macro
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@1164 47ffe4a8-6206-0410-815e-8f101de37557

--- IXMLPersist.h
+++ IXMLPersist.h
@@ -4,19 +4,19 @@
  * @brief IXMLPersist is a persistence interface.
  * @author Anar Manafov A.Manafov@gsi.de
  */ /*
- 
+
         version number:   $LastChangedRevision$
         created by:          Anar Manafov
                                   2006-07-15
         last changed by:   $LastChangedBy$ $LastChangedDate$
- 
+
         Copyright (c) 2006 GSI GridTeam. All rights reserved.
 ************************************************************************/
 #ifndef IXMLPERSIST_H
 #define IXMLPERSIST_H
 
 // Xerces-C++ headers
-#include <xercesc/dom/DOMNode.hpp> 
+#include <xercesc/dom/DOMNode.hpp>
 // GAW
 #include "ErrorCode.h"
 
@@ -45,9 +45,14 @@ namespace MiscCommon
             virtual ERRORCODE Write( xercesc::DOMNode* _element ) = 0;
     };
 
+
+    // TODO: Document me!
+#define DECLARE_XMLPERSIST_IMPL(_T) friend class MiscCommon::IXMLPersistImpl<_T>;
+    
+    // TODO: Document me!
     template <class _T>
     struct IXMLPersistImpl
-    { 
+    {
         void Read( xercesc::DOMNode* _element )
         {
             _T *pThis = reinterpret_cast<_T*>( this );
@@ -59,6 +64,32 @@ namespace MiscCommon
             pThis->WriteXmlCfg( _element );
         }
     };
+    
+    // TODO: Document me!
+    // #include "XMLHelper.h" needs to be added to the src. header (caller of BEGIN_READ_XML_CFG) 
+    
+#define BEGIN_READ_XML_CFG \
+    void ReadXmlCfg( xercesc::DOMNode* _element ) \
+    {   \
+        if ( !_element )    \
+            throw std::invalid_argument( "An internal error has been detected. Can't read configuration of JobManager manager, DOMNode is NULL." );  \
+    MiscCommon::XMLHelper::smart_XMLCh ElementName( "config" );    \
+        xercesc::DOMElement *config_element( dynamic_cast<xercesc::DOMElement* >( _element ) );   \
+        if ( !config_element )  \
+            throw std::runtime_error( "An internal error has been detected. Can't read configuration of JobManager manager, element \"config\" is missing" );    \
+            xercesc::DOMNodeList *list( config_element->getElementsByTagName( ElementName ) );   \
+            xercesc::DOMNode* node( list->item( 0 ) );   \
+            xercesc::DOMElement* elementConfig( NULL );  \
+        if ( xercesc::DOMNode::ELEMENT_NODE == node->getNodeType() ) \
+            elementConfig = dynamic_cast< xercesc::DOMElement* >( node ) ;   \
+        if ( !elementConfig )   \
+            throw std::runtime_error( "An internal error has been detected. Can't read configuration of JobManager manager, empty XML document" );
+    
+#define READ_ELEMENT( ELEMENT_NAME, VAR ) MiscCommon::XMLHelper::get_attr_value( elementConfig, #ELEMENT_NAME, &VAR );
+
+#define END_READ_XML_CFG }
+
+    
 };
 
 #endif
