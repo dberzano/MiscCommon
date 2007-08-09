bbc3096623e6bd7981c8b8dc398d0633deb21709
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-08-09 14:42:20 +0000

    PROOFAgent_modified: - catching several errors related to bad configuration file (errors in xml structure) - using only new schema of reading xml cfg: BEGIN/END_READ_XML_NODE... - many cosmetic changes
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@1167 47ffe4a8-6206-0410-815e-8f101de37557

--- IXMLPersist.h
+++ IXMLPersist.h
@@ -5,10 +5,10 @@
  * @author Anar Manafov A.Manafov@gsi.de
  */ /*
 
-        version number:   $LastChangedRevision$
-        created by:          Anar Manafov
-                                  2006-07-15
-        last changed by:   $LastChangedBy$ $LastChangedDate$
+        version number:     $LastChangedRevision$
+        created by:         Anar Manafov
+                            2006-07-15
+        last changed by:    $LastChangedBy$ $LastChangedDate$
 
         Copyright (c) 2006 GSI GridTeam. All rights reserved.
 ************************************************************************/
@@ -16,38 +16,14 @@
 #define IXMLPERSIST_H
 
 // MiscCommon
-#include "ErrorCode.h"
 #include "XMLHelper.h"
 
 namespace MiscCommon
 {
 
-    /**
-     * @brief an XML persistence interface
-     **/
-    class IXMLPersist
-    {
-        public:
-            virtual ~IXMLPersist()
-            {}
-            /**
-             * @brief Read data from the selected xml node.
-             * @param _element - [in] xml node to read
-             * @return MiscCommon::erXMLNullNode if \b _element is NULL and MiscCommon::erOK on success.
-             **/
-            virtual ERRORCODE Read( xercesc::DOMNode* _element ) = 0;
-            /**
-             * @brief Write data to the selected xml node.
-             * @param _element - [in] xml node to read
-             * @return MiscCommon::erXMLNullNode if \b _element is NULL, MiscCommon::erError in case of error and MiscCommon::erOK on success.
-             **/
-            virtual ERRORCODE Write( xercesc::DOMNode* _element ) = 0;
-    };
-
-
     // TODO: Document me!
 #define DECLARE_XMLPERSIST_IMPL(_T) friend class MiscCommon::IXMLPersistImpl<_T>;
-    
+
     // TODO: Document me!
     template <class _T>
     struct IXMLPersistImpl
@@ -63,38 +39,40 @@ namespace MiscCommon
             pThis->WriteXmlCfg( _element );
         }
     };
-    
+
     // TODO: Document me!
-#define BEGIN_READ_XML_CFG(_T) \
+#define BEGIN_READ_XML_NODE(_T, _ELEMENT_NAME) \
     void ReadXmlCfg( xercesc::DOMNode* _element ) \
     {   \
         const std::string str( "An internal error has been detected. Can't read configuration of "+ std::string(#_T) + " manager, " ); \
         if ( !_element )    \
             throw std::invalid_argument( str + "DOMNode is NULL." );  \
-        MiscCommon::XMLHelper::smart_XMLCh ElementName( "config" );    \
+        MiscCommon::XMLHelper::smart_XMLCh ElementName( _ELEMENT_NAME );    \
         xercesc::DOMElement *config_element( dynamic_cast<xercesc::DOMElement* >( _element ) );   \
         if ( !config_element )  \
-            throw std::runtime_error( str + "element \"config\" is missing" );    \
+            throw std::runtime_error( str + "element "+ std::string(#_ELEMENT_NAME)+" is missing" );    \
         xercesc::DOMNodeList *list( config_element->getElementsByTagName( ElementName ) );   \
         if( !list ) \
-            throw std::runtime_error( str + "element \"config\" is missing" );    \
+            throw std::runtime_error( str + "element "+ std::string(#_ELEMENT_NAME)+" is missing" );    \
         xercesc::DOMNode* node( list->item( 0 ) );   \
         if( !node ) \
-             throw std::runtime_error( str + "element \"config\" is missing" );    \
+            throw std::runtime_error( str + "element "+ std::string(#_ELEMENT_NAME)+" is missing" );    \
         xercesc::DOMElement* elementConfig( NULL );  \
         if ( xercesc::DOMNode::ELEMENT_NODE == node->getNodeType() ) \
             elementConfig = dynamic_cast< xercesc::DOMElement* >( node ) ;   \
         if ( !elementConfig )   \
             throw std::runtime_error( str + "empty XML document" );
-    
+
 #define READ_ELEMENT( ELEMENT_NAME, VAR ) MiscCommon::XMLHelper::get_attr_value( elementConfig, ELEMENT_NAME, &VAR );
 
+#define END_READ_XML_NODE }
+
+#define BEGIN_READ_XML_CFG(_T) BEGIN_READ_XML_NODE( _T, "config" )
 #define END_READ_XML_CFG }
-    
+
 #define BEGIN_WRITE_XML_CFG(_T) void WriteXmlCfg( xercesc::DOMNode* _element ) {
 #define END_WRITE_XML_CFG }
 
-    
 };
 
 #endif
