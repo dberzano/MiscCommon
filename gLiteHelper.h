1034109456bead08fb62c933a21a96d067157fb6
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2007-08-16 13:20:53 +0000

    GAW_modified: - usual revision of the code. - JobManager methods have been revised, cleaned, and optimized - cosmetic changes
    git-svn-id: https://subversion.gsi.de/dgrid/D-Grid/Include@1203 47ffe4a8-6206-0410-815e-8f101de37557

--- gLiteHelper.h
+++ gLiteHelper.h
@@ -20,32 +20,60 @@
 #include "glite/wmsutils/jobid/JobId.h"
 // MiscCommon
 #include "def.h"
+#include "BOOST_FILESYSTEM.h"
 
-class CJobStatusObj
+namespace MiscCommon
 {
-    public:
-        CJobStatusObj( const std::string &_gLiteJobID, int _Flag = glite::lb::Job::STAT_CLASSADS |  glite::lb::Job::STAT_CHILDSTAT | glite::lb::Job::STAT_CHILDREN )
+    namespace gLite
+    {
+
+        class CJobStatusObj
         {
-            glite::wmsutils::jobid::JobId jobid( _gLiteJobID );
-            glite::lb::Job job( jobid );
-            m_status = job.status( _Flag );
+            public:
+                CJobStatusObj( const std::string &_gLiteJobID, int _Flag = glite::lb::Job::STAT_CLASSADS |  glite::lb::Job::STAT_CHILDSTAT | glite::lb::Job::STAT_CHILDREN )
+                {
+                    glite::wmsutils::jobid::JobId jobid( _gLiteJobID );
+                    glite::lb::Job job( jobid );
+                    m_status = job.status( _Flag );
+                }
+
+                glite::lb::JobStatus &Get()
+                {
+                    return m_status;
+                }
+
+                void GetChildren( MiscCommon::StringVector_t *_Container ) const throw (std::exception)
+                {
+                    if ( !_Container )
+                        throw std::invalid_argument( "CJobStatusObj:GetChildren: Argument is NULL." );
+
+                    *_Container = m_status.getValStringList( glite::lb::JobStatus::CHILDREN );
+                }
+
+            private:
+                glite::lb::JobStatus m_status;
+        };
+        
+        inline std::string get_globus_url_copy_exe() throw(std::exception)
+        {
+          // Defining the path to the globus-url-copy executable
+          // 1. Trying $GLOBUS_LOCATION
+          // 1.1. using  $GLOBUS_LOCAT/bin/globus-url-copy
+          // OR
+          // 2. if exists using /opt/globus/bin/globus-url-copy
+          // OR
+          // runtime_error("Unable to find globus-url-copy executable");
+          const std::string globurlcp( "globus-url-copy" );
+          if ( getenv("GLOBUS_LOCATION") )
+              return ( std::string(getenv("GLOBUS_LOCATION")) + "/bin/" + globurlcp );
+          else if ( is_directory ("/opt/globus/bin") )
+              return ( "/opt/globus/bin/" + globurlcp );
+          else
+              throw std::runtime_error("Unable to find globus-url-copy executable");          
         }
 
-        glite::lb::JobStatus &Get()
-        {
-            return m_status;
-        }
-
-        void GetChildren( MiscCommon::StringVector_t *_Container ) throw (std::exception)
-        {
-            if ( !_Container )
-                throw std::invalid_argument( "CJobStatusObj:GetChildren: Argument is NULL." );
-
-            *_Container = m_status.getValStringList( glite::lb::JobStatus::CHILDREN );
-        }
-
-    private:
-        glite::lb::JobStatus m_status;
+    };
 };
+
 
 #endif /*GLITEHELPER_H_*/
