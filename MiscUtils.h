e59f2c2b953106d9b879df148a8fdc67f7d7f6b9
manafov <manafov@47ffe4a8-6206-0410-815e-8f101de37557>
2008-01-03 12:30:06 +0000

    MiscCommon_added: select1st and select2nd function objects
    git-svn-id: https://subversion.gsi.de/dgrid/Include@1603 47ffe4a8-6206-0410-815e-8f101de37557

--- MiscUtils.h
+++ MiscUtils.h
@@ -5,10 +5,10 @@
  * @author Anar Manafov A.Manafov@gsi.de
  */ /*
 
-        version number:     $LastChangedRevision$
+        version number:     $LastChangedRevision:1599 $
         created by:         Anar Manafov
                             2006-03-28
-        last changed by:    $LastChangedBy$ $LastChangedDate$
+        last changed by:    $LastChangedBy:manafov $ $LastChangedDate:2008-01-02 15:12:05 +0100 (Wed, 02 Jan 2008) $
 
         Copyright (c) 2006, 2007 GSI GridTeam. All rights reserved.
 ************************************************************************/
@@ -19,16 +19,16 @@
 #include <algorithm>
 
 /**
- * 
+ *
  *  @brief Miscellaneous functions and helpers are located here
- * 
+ *
  */
 namespace MiscCommon
 {
     /**
-     * 
+     *
      * @brief Class which makes child to be non-copyable object.
-     * 
+     *
      */
     class NONCopyable
     {
@@ -41,12 +41,12 @@ namespace MiscCommon
             NONCopyable( const NONCopyable& );
             const NONCopyable& operator=( const NONCopyable& );
     };
-    
+
     class NullType {};
     /**
-     * 
+     *
      * @brief A helper class. Helps to automatically track environment variables.
-     * 
+     *
      */
     class auto_setenv
     {
@@ -76,7 +76,7 @@ namespace MiscCommon
             void set( const std::string &_VarName, const std::string &_NewValue )
             {
                 unset();
-                
+
                 m_sVarName = _VarName;
                 m_sNewValue = _NewValue;
 
@@ -93,7 +93,7 @@ namespace MiscCommon
                 if ( m_unset )
                 {
                     m_unset = false;
-                    unsetenv( m_sVarName.c_str() );                    
+                    unsetenv( m_sVarName.c_str() );
                     return;
                 }
 
@@ -112,12 +112,12 @@ namespace MiscCommon
     };
 
     /**
-     * 
+     *
      *  @brief appends character _ItemToAdd to the string _pString if there is no such suffix on the end of _pString.
      *  @param[in] _pString - The string to be processed.
      *  @param[in] _ItemToAdd - The target characters to be checked and added.
      *  @return A pointer to the processed string object.
-     * 
+     *
      */
     template <typename _T>
     _T* smart_append( _T* _pString, const typename _T::value_type _ItemToAdd )
@@ -132,12 +132,12 @@ namespace MiscCommon
     }
 
     /**
-     * 
+     *
      *  @brief trims trailing and leading characters from the string.
      *  @param[in] _pString - The string to be trimmed.
      *  @param[in] _chWhat - The target character to be trimmed.
      *  @return A reference to the string object from which the elements have been trimmed.
-     * 
+     *
      */
     template <typename _T>
     _T& trim( _T* _pString, const typename _T::value_type& _chWhat )
@@ -146,12 +146,12 @@ namespace MiscCommon
     }
 
     /**
-     * 
+     *
      *  @brief trims trailing characters from the string.
      *  @param[in] _pString - The string to be trimmed.
      *  @param[in] _chWhat - The target character to be trimmed.
      *  @return A reference to the string object from which the elements have been trimmed.
-     * 
+     *
      */
     template <typename _T>
     _T& trim_right( _T* _pString, const typename _T::value_type& _chWhat )
@@ -160,12 +160,12 @@ namespace MiscCommon
     }
 
     /**
-     * 
+     *
      *  @brief trims leading characters from the string.
      *  @param[in] _pString - The string to be trimmed.
      *  @param[in] _chWhat - The target character to be trimmed.
      *  @return A reference to the string object from which the elements have been trimmed.
-     * 
+     *
      */
     template <typename _T>
     _T& trim_left( _T* _pString, const typename _T::value_type& _chWhat )
@@ -174,13 +174,13 @@ namespace MiscCommon
     }
 
     /**
-     * 
+     *
      *  @brief finds elements in a string match a specified string and replaces it.
      *  @param[in,out] _pString - The string to be processed.
      *  @param[in,out] _what - String to be replaced.
      *  @param[in] _with - Replacing string.
      *  @return A reference to the string object from which the elements have been replaced.
-     * 
+     *
      */
     template <typename _T>
     _T& replace( _T* _pString, const _T& _what, const _T& _with )
@@ -221,11 +221,11 @@ namespace MiscCommon
     };
 
     /**
-     * 
+     *
      *  @brief convert string to upper case.
      *  @param[in] _str - Sting to convert.
      *  @return Converted string.
-     * 
+     *
      */
     template <typename _T>
     _T& to_upper( _T& _str )
@@ -235,11 +235,11 @@ namespace MiscCommon
     }
 
     /**
-     * 
+     *
      *  @brief convert string to lower case.
      *  @param[in] _str - Sting to convert.
      *  @return Converted string.
-     * 
+     *
      */
     template <typename _T>
     _T& to_lower( _T& _str )
@@ -247,6 +247,40 @@ namespace MiscCommon
         std::transform( _str.begin(), _str.end(), _str.begin(), ToLower() );
         return _str;
     }
+    /*
+     * 
+     * @brief select1st is a function object that takes a single argument, a pair, and returns the pair's first element.
+     * 
+     */
+    template <class _Pair>
+    struct select1st : public unary_function<_Pair, typename _Pair::first_type>
+    {
+        typename _Pair::first_type& operator()(_Pair& __x) const
+        {
+            return __x.first;
+        }
+        const typename _Pair::first_type& operator()(const _Pair& __x) const
+        {
+            return __x.first;
+        }
+    };
+    /**
+     * 
+     * @brief select2nd is a function object that takes a single argument, a pair, and returns the pair's second element.
+     * 
+     */
+    template <class _Pair>
+    struct select2nd : public unary_function<_Pair, typename _Pair::second_type>
+    {
+        typename _Pair::second_type& operator()(_Pair& __x) const
+        {
+            return __x.second;
+        }
+        const typename _Pair::second_type& operator()(const _Pair& __x) const
+        {
+            return __x.second;
+        }
+    };
 
 };
 #endif
