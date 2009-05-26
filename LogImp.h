d79803b7f9aa85c576f20cca072f90f4753085a8
Anar Manafov <anar@localhost.localdomain>
2009-05-26 18:55:02 +0200

    fixed a typo

--- LogImp.h
+++ LogImp.h
@@ -41,35 +41,35 @@ namespace MiscCommon
      * @brief ofstream specialization of CLog
      *
      */
-    class CLogSinglton
+    class CLogSingleton
     {
             typedef std::auto_ptr<CFileLog> CFileLogPtr;
 
-            CLogSinglton()
+            CLogSingleton()
             {}
-            ~CLogSinglton()
+            ~CLogSingleton()
             {}
 
         public:
             int Init( const std::string &_LogFileName, bool _CreateNew = false )
             {
                 if ( m_log.get() )
-                    throw std::logic_error("CLogSinglton error: singleton class has been already initialized.");
+                    throw std::logic_error( "CLogSinglton error: singleton class has been already initialized." );
 
                 m_log = CFileLogPtr( new CFileLog( _LogFileName, _CreateNew ) );
                 push( LOG_SEVERITY_INFO, 0, "LOG singleton", "LOG singleton has been initialized." );
                 return 0;
             }
-            static CLogSinglton &Instance()
+            static CLogSingleton &Instance()
             {
-                static CLogSinglton log;
+                static CLogSingleton log;
                 return log;
             }
             CFileLog::stream_type &push( LOG_SEVERITY _Severity, unsigned long _ErrorCode, const std::string &_Module, const std::string &_Message )
             {
                 if ( !m_log.get() )
                 {
-                    return reinterpret_cast<CFileLog::stream_type &>(std::cerr << _Message << std::endl);
+                    return reinterpret_cast<CFileLog::stream_type &>( std::cerr << _Message << std::endl );
                 }
                 return m_log->push( _Severity, _ErrorCode, _Module, _Message );
             }
@@ -104,42 +104,42 @@ namespace MiscCommon
         public:
             CLogImp()
             {
-                if ( CLogSinglton::Instance().IsReady() )
-                    CLogSinglton::Instance().push( LOG_SEVERITY_INFO, 0, g_cszMODULENAME_CORE, "Bringing >>> " + GetModuleName() + " <<< to life..." );
+                if ( CLogSingleton::Instance().IsReady() )
+                    CLogSingleton::Instance().push( LOG_SEVERITY_INFO, 0, g_cszMODULENAME_CORE, "Bringing >>> " + GetModuleName() + " <<< to life..." );
             }
             ~CLogImp()
             {
-                CLogSinglton::Instance().push( LOG_SEVERITY_INFO, 0, g_cszMODULENAME_CORE, "Shutting down >>> " + GetModuleName() + " <<<" );
+                CLogSingleton::Instance().push( LOG_SEVERITY_INFO, 0, g_cszMODULENAME_CORE, "Shutting down >>> " + GetModuleName() + " <<<" );
             }
             CFileLog::stream_type &InfoLog( const std::string &_Message )
             {
-                return CLogSinglton::Instance().push( LOG_SEVERITY_INFO, 0, GetModuleName(), _Message );
+                return CLogSingleton::Instance().push( LOG_SEVERITY_INFO, 0, GetModuleName(), _Message );
             }
             CFileLog::stream_type &InfoLog( unsigned long _ErrorCode, const std::string &_Message )
             {
-                return CLogSinglton::Instance().push( LOG_SEVERITY_INFO, _ErrorCode, GetModuleName(), _Message );
+                return CLogSingleton::Instance().push( LOG_SEVERITY_INFO, _ErrorCode, GetModuleName(), _Message );
             }
             CFileLog::stream_type &WarningLog( unsigned long _ErrorCode, const std::string &_Message )
             {
-                return CLogSinglton::Instance().push( LOG_SEVERITY_WARNING, _ErrorCode, GetModuleName(), _Message );
+                return CLogSingleton::Instance().push( LOG_SEVERITY_WARNING, _ErrorCode, GetModuleName(), _Message );
             }
             CFileLog::stream_type &FaultLog( unsigned long _ErrorCode, const std::string &_Message )
             {
-                return CLogSinglton::Instance().push( LOG_SEVERITY_FAULT, _ErrorCode, GetModuleName(), _Message );
+                return CLogSingleton::Instance().push( LOG_SEVERITY_FAULT, _ErrorCode, GetModuleName(), _Message );
             }
             CFileLog::stream_type &CriticalErrLog( unsigned long _ErrorCode, const std::string &_Message )
             {
-                return CLogSinglton::Instance().push( LOG_SEVERITY_CRITICAL_ERROR, _ErrorCode, GetModuleName(), _Message );
+                return CLogSingleton::Instance().push( LOG_SEVERITY_CRITICAL_ERROR, _ErrorCode, GetModuleName(), _Message );
             }
             CFileLog::stream_type &DebugLog( unsigned long _ErrorCode, const std::string &_Message )
             {
-                return CLogSinglton::Instance().push( LOG_SEVERITY_DEBUG, _ErrorCode, GetModuleName(), _Message );
+                return CLogSingleton::Instance().push( LOG_SEVERITY_DEBUG, _ErrorCode, GetModuleName(), _Message );
             }
 
         private:
             std::string GetModuleName()
             {
-                _T *pT = reinterpret_cast<_T*>(this);
+                _T *pT = reinterpret_cast<_T*>( this );
                 return pT->GetModuleName();
             }
     };
