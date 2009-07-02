22b67be31b09cab60fba59c18d25420977b8c1f5
Anar Manafov <anar@localhost.localdomain>
2009-07-02 19:13:13 +0200

    added the PoD user-defaults description file

---
+++ PoDUserDefaultsOptions.h
@@ -0,0 +1,20 @@
+/*
+ * PoDUserDefaultsOptions.h
+ *
+ *  Created on: Jun 30, 2009
+ *      Author: Anar Manafov
+ */
+
+#ifndef PODUSERDEFAULTSOPTIONS_H_
+#define PODUSERDEFAULTSOPTIONS_H_
+
+namespace PoD
+{
+
+    typedef struct SPoDUserDefaultOptions
+    {
+    }SPoDUserDefaultsOptions_t;
+
+}
+
+#endif /* PODUSERDEFAULTSOPTIONS_H_ */
