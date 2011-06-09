f64208bcaebaf2c796f0e139745ae8e47d2a3002
Anar Manafov <A.Manafov@gsi.de>
2011-06-09 17:39:33 +0200

    introduce the pod_sys_files library. The library contains references to all configuration, support and pod system files.

---
+++ pod_sys_files/PoDSysFiles.h
@@ -0,0 +1,139 @@
+//
+//  PoDSysFiles.h
+//  PoD
+//
+//  Created by Anar Manafov on 09.06.11.
+//  Copyright 2011 GSI. All rights reserved.
+//
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+//=============================================================================
+// STD
+#include <string>
+// MiscCommon
+#include "PoDUserDefaultsOptions.h"
+//=============================================================================
+class CEnvironment
+{
+    public:
+        CEnvironment();
+        ~CEnvironment();
+
+    public:
+        void init();
+
+        std::string version() const
+        {
+            return m_localVer;
+        }
+        std::string PoDPath() const
+        {
+            return m_PoDPath;
+        }
+        std::string serverHost() const
+        {
+            return m_srvHost;
+        }
+        unsigned int serverPort() const
+        {
+            return m_srvPort;
+        }
+        const PoD::SPoDUserDefaultsOptions_t getUD() const
+        {
+            return *m_ud;
+        }
+        std::string srvInfoFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "etc/server_info.cfg";
+            return ret;
+        }
+        std::string srvInfoFileRemote() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "etc/remote_server_info.cfg";
+            return ret;
+        }
+        std::string tunnelXpdPidFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "rmt_srv_tunnel_xpd.pid";
+            return ret;
+        }
+        std::string tunnelAgentPidFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "rmt_srv_tunnel_agent.pid";
+            return ret;
+        }
+        std::string pod_remotePidFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "pod-remote.pid";
+            return ret;
+        }
+        std::string xpdCfgFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "etc/xpd.cf";
+            return ret;
+        }
+        std::string agentPidFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "pod-agent.pid";
+            return ret;
+        }
+        std::string pipe_log_enginePipeFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += ".pod_remote_pipe";
+            return ret;
+        }
+        std::string pod_remoteCfgFile() const
+        {
+            if( m_wrkDir.empty() )
+                return( "" );
+
+            std::string ret( m_wrkDir );
+            ret += "etc/pod-remote.cfg";
+            return ret;
+        }
+
+    private:
+        void _localVersion();
+
+    private:
+        std::string m_PoDPath;
+        std::string m_localVer;
+        std::string m_srvHost;
+        unsigned int m_srvPort;
+        PoD::SPoDUserDefaultsOptions_t *m_ud;
+        std::string m_wrkDir;
+};
+
+#endif
