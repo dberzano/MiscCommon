c48beead90c31a3adff18e58bd0e6ab4c1af61fd
Anar Manafov <Anar.Manafov@gmail.com>
2013-07-16 16:40:26 +0200

    Added: new config options "server.gen_temp_ssh_keys"

--- PoDUserDefaultsOptions.h
+++ PoDUserDefaultsOptions.h
@@ -75,6 +75,7 @@ namespace PoD
         std::string m_packetForwarding;
         unsigned int m_agentThreads;    //!< A number of threads in thread pool.
         std::string m_proofCfgEntryPattern;
+        bool m_genTempSSHKeys; //!< Indicates, whether PoD need to generate temporary SSH keys for WNs. Useful in case PoD server has only ssh port open for incoming connection.
     } SServerOptions_t;
 
     typedef struct SWorkerOptions
@@ -194,6 +195,7 @@ namespace PoD
                 ( "server.packet_forwarding", boost::program_options::value<std::string>( &m_options.m_server.m_packetForwarding )->default_value( "auto" ), "" )
                 ( "server.proof_cfg_entry_pattern",
                   boost::program_options::value<std::string>( &m_options.m_server.m_proofCfgEntryPattern )->default_value( "worker %user%@%host% port=%port% pref=100" ), "" )
+                ( "server.gen_temp_ssh_keys", boost::program_options::value<bool>( &m_options.m_server.m_genTempSSHKeys )->default_value( false ), "" )
                 ;
                 config_file_options.add_options()
                 ( "worker.work_dir", boost::program_options::value<std::string>( &m_options.m_worker.m_common.m_workDir )->default_value( "$POD_LOCATION/" ), "" )
@@ -298,7 +300,8 @@ namespace PoD
                         << "agent_threads=" << ud.getValueForKey( "server.agent_threads" ) << "\n"
                         << "agent_node_readbuffer=" << ud.getValueForKey( "server.agent_node_readbuffer" ) << "\n"
                         << "packet_forwarding=" << ud.getValueForKey( "server.packet_forwarding" ) << "\n"
-                        << "proof_cfg_entry_pattern=" << ud.getValueForKey( "server.proof_cfg_entry_pattern" ) << "\n";
+                        << "proof_cfg_entry_pattern=" << ud.getValueForKey( "server.proof_cfg_entry_pattern" ) << "\n"
+                        << "server.gen_temp_ssh_keys=" << ud.getUnifiedBoolValueForBoolKey( "server.gen_temp_ssh_keys" ) << "\n";
                 _stream
                         << "[worker]\n"
                         << "work_dir=" << ud.getValueForKey( "worker.work_dir" ) << "\n"
